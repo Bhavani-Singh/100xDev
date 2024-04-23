001d43908f5b1e00ddbb3fdc34ff8c81d2de86eb
Bhavani-Singh <bhavanisinghrajput24@gmail.com>
2024-04-23 22:37:45 +0530

    implement admin functionalities

--- week-3/04-mongo-with-jwt-auth/middleware/admin.js
+++ week-3/04-mongo-with-jwt-auth/middleware/admin.js
@@ -5,9 +5,12 @@ const jwtPassword = 'secret';
 function adminMiddleware(req, res, next) {
     // Implement admin auth logic
     // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
-    const token = req.header.Authorization;
+    const token = req.headers.authorization;
+    console.log(token);
 
     const decode =jwt.decode(token, jwtPassword);
+
+    // console.log(decode);
     
     if(decode) {
         next();
--- week-3/04-mongo-with-jwt-auth/routes/admin.js
+++ week-3/04-mongo-with-jwt-auth/routes/admin.js
@@ -8,7 +8,7 @@ const jwtPassword = 'secret';
 // Admin Routes
 router.post('/signup', (req, res) => {
     // Implement admin signup logic
-    const {username, password} = req.query;
+    const {username, password} = req.body;
 
     Admin.create({username, password})
     .then((result) => {
@@ -27,7 +27,8 @@ router.post('/signup', (req, res) => {
 
 router.post('/signin', (req, res) => {
     // Implement admin signup logic
-    const {username, password} = req.query;
+    const {username, password} = req.body;
+    
     Admin.findOne({username})
     .then(result => {
         if(result !== null && result.password === password) {
@@ -52,11 +53,21 @@ router.post('/signin', (req, res) => {
 
 router.post('/courses', adminMiddleware, (req, res) => {
     // Implement course creation logic
-    const {title, description, price, imageLink} = req.query;
+    const {title, description, price, imageLink} = req.body;
 
     Course.create({title, description, price, imageLink})
     .then(result => {
-        console.log(result);
+        if(result !== null) {
+            res.status(200).json({
+                message: 'Course created successfully', 
+                couseId: result._id
+            })
+        }
+    })
+    .catch(error => {
+        res.status(400).json({
+            message: 'Error while Creating the course'
+        })
     })
 
 
@@ -64,6 +75,17 @@ router.post('/courses', adminMiddleware, (req, res) => {
 
 router.get('/courses', adminMiddleware, (req, res) => {
     // Implement fetching all courses logic
+    Course.find({})
+    .then(result => {
+        res.status(200).json({
+            result
+       })
+    })
+    .catch(error => {
+        res.status(500).json({
+            message: 'Error while fetching the courses'
+        })
+    })
 });
 
 module.exports = router;
